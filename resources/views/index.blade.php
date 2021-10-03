5f48e1963505a638a51714422818fec6f4508d06
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-10-03 23:03:40 +0900

    create project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<?php
+
+// var_dump($posts);
+// exit;
+
+dd($posts);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            <li>Title</li>
+            <li>Title</li>
+            <li>Title</li>
+        </ul>
+    </div>
+</body>
+</html>
